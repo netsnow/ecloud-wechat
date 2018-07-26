ccbe4a36b4b10ead6e7c6323d7e5c0b2a2bcca86
baixue <bai.xue@me.com>
2018-07-26 15:25:40 +0800

    add new function 'leave'

---
+++ page/bussiness/leave/leave.js
@@ -0,0 +1,119 @@
+var app = getApp()
+const requestUrl = require('../../../config').requestUrl
+const applicationId = require('../../../config').applicationId
+const restApiKey = require('../../../config').restApiKey
+
+Page({
+  data: {
+  },
+  onShow: function () {
+    if (!app.globalData.hasLogin) {
+      wx.redirectTo({ url: '../login/login' })
+      //console.log("123");
+    }
+  },
+  apply: function (memo) {
+    //console.log(app.globalData.openid)
+    wx.request({
+      url: requestUrl + '/1/classes/userinfo?where=%7B%22wechatNickName%22:%22' + app.globalData.openid+'%22%7D',
+      header: {
+        'Content-Type': 'application/json',
+        'X-Bmob-Application-Id': applicationId,
+        'X-Bmob-REST-API-Key': restApiKey,
+      },
+      data: {
+      },
+      success: function (result) {
+        if(result.data.results.length == 0){
+          wx.showToast({
+            title: '请补全用户信息',
+            icon: 'none',
+            mask: true,
+            duration: 2000
+          })
+        }else{
+          var userObjectId = result.data.results[0].objectId
+          var day = new Date()
+          day.setTime(day.getTime())
+          var today = day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDate()
+
+          wx.request({
+            url: requestUrl + '/1/classes/leave?where=%7B%22date%22:%22' + today + '%22%7D',
+            header: {
+              'Content-Type': 'application/json',
+              'X-Bmob-Application-Id': applicationId,
+              'X-Bmob-REST-API-Key': restApiKey,
+            },
+            data: {
+              'include': 'user'
+            },
+            success: function (result) {
+              var list = result.data.results
+              var isExist = false
+              for (var i = 0, len = list.length; i < len; ++i) {
+                if (list[i].user.wechatNickName == app.globalData.openid) {
+                  isExist = true
+                }
+              }
+              if(isExist){
+                wx.showToast({
+                  title: '今天已请过假！',
+                  icon: 'none',
+                  mask: true,
+                  duration: 2000
+                })
+              }else{
+                wx.request({
+                  url: requestUrl + '/1/classes/leave',
+                  header: {
+                    'Content-Type': 'application/json',
+                    'X-Bmob-Application-Id': applicationId,
+                    'X-Bmob-REST-API-Key': restApiKey,
+                  },
+                  method: 'POST',
+                  data: {
+                    user: {
+                      "__type": "Pointer",
+                      "className": "_User",
+                      "objectId": userObjectId
+                    },
+                    leaveType: memo,
+                    date: today,
+                  },
+                  success: function (result) {
+                    wx.showToast({
+                      title: '请假成功！',
+                      icon: 'success',
+                      mask: true,
+                      duration: 2000
+                    })
+                  },
+                  fail: function ({ errMsg }) {
+                    wx.showToast({
+                      title: '请假失败！',
+                      icon: 'none',
+                      mask: true,
+                      duration: 2000
+                    })
+                  }
+                })
+
+              }
+              console.log('request success', result)
+            }
+          })
+        }
+        //console.log('request success', result)
+      },
+      fail: function ({ errMsg }) {
+        console.log('request fail', errMsg)
+      }
+    })
+  },
+  delay: function (e) {
+    this.apply("迟到");
+  },
+  leave: function (e) {
+    this.apply("请假");
+  }
+})
